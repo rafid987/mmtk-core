7d6a64149847fa0c63b5bcd59b9c3535c8739928
rafid987 <69402023+rafid987@users.noreply.github.com>
2023-04-09 17:06:55 +0600

    Update mutator.rs

--- src/plan/mygc/mutator.rs
+++ src/plan/mygc/mutator.rs
@@ -4,7 +4,7 @@ use crate::plan::mutator_context::MutatorConfig;
 use crate::plan::mutator_context::{
     create_allocator_mapping, create_space_mapping, ReservedAllocators,
 };
-use crate::plan::nogc::NoGC;
+use crate::plan::mygc::MyGC;
 use crate::plan::AllocationSemantics;
 use crate::plan::Plan;
 use crate::util::alloc::allocators::{AllocatorSelector, Allocators};
@@ -12,19 +12,19 @@ use crate::util::{VMMutatorThread, VMWorkerThread};
 use crate::vm::VMBinding;
 use enum_map::{enum_map, EnumMap};
 
-/// We use three bump allocators when enabling nogc_multi_space.
+/// We use three bump allocators when enabling mygc_multi_space.
 const MULTI_SPACE_RESERVED_ALLOCATORS: ReservedAllocators = ReservedAllocators {
     n_bump_pointer: 3,
     ..ReservedAllocators::DEFAULT
 };
 
 lazy_static! {
-    /// When nogc_multi_space is disabled, force all the allocation go to the default allocator and space.
+    /// When mygc_multi_space is disabled, force all the allocation go to the default allocator and space.
     static ref ALLOCATOR_MAPPING_SINGLE_SPACE: EnumMap<AllocationSemantics, AllocatorSelector> = enum_map! {
         _ => AllocatorSelector::BumpPointer(0),
     };
     pub static ref ALLOCATOR_MAPPING: EnumMap<AllocationSemantics, AllocatorSelector> = {
-        if cfg!(feature = "nogc_multi_space") {
+        if cfg!(feature = "mygc_multi_space") {
             let mut map = create_allocator_mapping(MULTI_SPACE_RESERVED_ALLOCATORS, false);
             map[AllocationSemantics::Default] = AllocatorSelector::BumpPointer(0);
             map[AllocationSemantics::Immortal] = AllocatorSelector::BumpPointer(1);
@@ -36,11 +36,11 @@ lazy_static! {
     };
 }
 
-pub fn nogc_mutator_noop<VM: VMBinding>(_mutator: &mut Mutator<VM>, _tls: VMWorkerThread) {
+pub fn mygc_mutator_noop<VM: VMBinding>(_mutator: &mut Mutator<VM>, _tls: VMWorkerThread) {
     unreachable!();
 }
 
-pub fn create_nogc_mutator<VM: VMBinding>(
+pub fn create_mygc_mutator<VM: VMBinding>(
     mutator_tls: VMMutatorThread,
     plan: &'static dyn Plan<VM = VM>,
 ) -> Mutator<VM> {
@@ -50,20 +50,20 @@ pub fn create_nogc_mutator<VM: VMBinding>(
             let mut vec = create_space_mapping(MULTI_SPACE_RESERVED_ALLOCATORS, false, plan);
             vec.push((
                 AllocatorSelector::BumpPointer(0),
-                &plan.downcast_ref::<NoGC<VM>>().unwrap().nogc_space,
+                &plan.downcast_ref::<MyGC<VM>>().unwrap().mygc_space,
             ));
             vec.push((
                 AllocatorSelector::BumpPointer(1),
-                &plan.downcast_ref::<NoGC<VM>>().unwrap().immortal,
+                &plan.downcast_ref::<MyGC<VM>>().unwrap().immortal,
             ));
             vec.push((
                 AllocatorSelector::BumpPointer(2),
-                &plan.downcast_ref::<NoGC<VM>>().unwrap().los,
+                &plan.downcast_ref::<MyGC<VM>>().unwrap().los,
             ));
             vec
         }),
-        prepare_func: &nogc_mutator_noop,
-        release_func: &nogc_mutator_noop,
+        prepare_func: &mygc_mutator_noop,
+        release_func: &mygc_mutator_noop,
     };
 
     Mutator {
