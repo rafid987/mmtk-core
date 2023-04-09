cf5ff8e13ad402f98304101b4a17b0d64beabb67
rafid987 <69402023+rafid987@users.noreply.github.com>
2023-04-09 17:03:35 +0600

    Update mod.rs

--- src/plan/mygc/mod.rs
+++ src/plan/mygc/mod.rs
@@ -1,7 +1,7 @@
-//! Plan: nogc (allocation-only)
+//! Plan: mygc (allocation-only)
 
 pub(super) mod global;
 pub(super) mod mutator;
 
-pub use self::global::NoGC;
-pub use self::global::NOGC_CONSTRAINTS;
+pub use self::global::MyGC;
+pub use self::global::MyGC_CONSTRAINTS;
